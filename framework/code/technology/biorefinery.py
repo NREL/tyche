b30b812ab7ef93efbe3c18bb79239c4d007d6f1e
rjhanes <rebecca.hanes@nrel.gov>
2020-03-31 09:53:21 -0600

    creates empty file for implementing biorefinery production function model

---
+++ framework/code/technology/biorefinery.py
@@ -0,0 +1,21 @@
+# Biorefinery model with four processing steps
+
+
+# All of the computations must be vectorized, so use `numpy`.
+import numpy as np
+
+
+# Production function.
+def production(capital, fixed, input, parameter):
+
+  pass
+
+  # Package results.
+  return None
+
+
+# Metrics function.
+def metrics(capital, fixed, input, outputs, parameter):
+    pass
+
+    return None
