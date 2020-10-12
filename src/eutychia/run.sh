c73122ccb61066e67e83f7094f4508c726160b0c
Brian W Bush <brian.bush@nrel.gov>
2020-10-12 15:20:20 -0600

    turned on logging

--- src/eutychia/run.sh
+++ src/eutychia/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/5272327b81ed355bbed5659b8d303cf2979b6953.tar.gz
-#!nix-shell --run "hypercorn --certfile certificate.crt --keyfile private.key  --bind 0.0.0.0:5000 main:app" -- ../../shell.nix
+#!nix-shell --run "hypercorn --access-logfile - --error-logfile - --certfile certificate.crt --keyfile private.key --bind 0.0.0.0:5000 main:app" -- ../../shell.nix
