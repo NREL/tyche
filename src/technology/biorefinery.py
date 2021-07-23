a74cf9d93d54fb897dbf36b4e3f9da69d533e4cf
rjhanes <rebecca.hanes@nrel.gov>
2021-07-23 12:29:13 -0600

    updated input files, code and notebook for biorefinery example before testing

--- src/technology/biorefinery.py
+++ src/technology/biorefinery.py
@@ -17,13 +17,24 @@ def capital_cost(scale, parameter):
       The scale of operation.
     parameter : array
       The technological parameterization.
+
+    Returns
+    -------
+    Total capital cost for one biorefinery (USD/biorefinery)
     """
-    return np.stack([
-        parameter[3],
-        parameter[4],
-        parameter[5],
-        parameter[6],
-    ])
+    # "original" biorefinery scale
+    o_scale = parameter[10]
+
+    # scaling factor for equipment costs
+    scale_factor = parameter[11]
+
+    pre = parameter[3] * (scale / o_scale) ** scale_factor
+    fer = parameter[4] * (scale / o_scale) ** scale_factor
+    con = parameter[5] * (scale / o_scale) ** scale_factor
+    sep = parameter[6] * (scale / o_scale) ** scale_factor
+    utl = parameter[7] * (scale / o_scale) ** scale_factor
+
+    return np.stack([pre, fer, con, sep, utl])
 
 
 def fixed_cost(scale, parameter):
@@ -32,15 +43,20 @@ def fixed_cost(scale, parameter):
 
     Parameters
     ----------
-    scale : float
+    scale : float [Unused]
       The scale of operation.
     parameter : array
       The technological parameterization.
+
+    Returns
+    -------
+    total fixed costs for one biorefinery (USD/year)
     """
-    return np.stack([
-        parameter[7],
-        parameter[8],
-    ])
+    o_scale = parameter[10]
+    rnt = parameter[8] * (scale / o_scale)
+    ins = parameter[9] * (scale / o_scale)
+
+    return np.stack([rnt, ins])
 
 
 def production(scale, capital, lifetime, fixed, input, parameter):
@@ -58,19 +74,25 @@ def production(scale, capital, lifetime, fixed, input, parameter):
     fixed : array
       Fixed costs.
     input : array
-      Input quantities. 
+      Input quantities.
     parameter : array
       The technological parameterization.
+
+    Returns
+    -------
+    output_raw
+        Ideal/theoretical production of each technology output: biofuel at
+        gals/year
     """
 
-    # input efficiency (preprocessing efficiency) is accounted for in Designs.py
-    # ditto output efficiency (separation efficiency)
-    # so only fermentation efficiency (parameter[0]) and conversion efficiency
-    # (parameter[1]) is seen in this equation
-    out = input[0] * parameter[0] * parameter[1]
+    theor_yield = parameter[12]
+    feedstock = input[0] * scale
 
-    # Package results.
-    return out
+    # theoretical biofuel yield w/out any efficiency losses
+    output_raw = theor_yield * feedstock
+
+    return np.stack([output_raw])
+
 
 
 def metrics(scale, capital, lifetime, fixed, input_raw, input, output_raw, output, cost, parameter):
@@ -80,25 +102,40 @@ def metrics(scale, capital, lifetime, fixed, input_raw, input, output_raw, outpu
     Parameters
     ----------
     scale : float
-      The scale of operation.
+      The scale of operation. Unitless
     capital : array
-      Capital costs.
+      Capital costs. Units: USD/biorefinery
     lifetime : float
-      Technology lifetime.
+      Technology lifetime. Units: year
     fixed : array
-      Fixed costs.
+      Fixed costs. Units: USD/year
     input_raw : array
-      Raw input quantities (before losses).
+      Raw input quantities (before losses). Units: metric ton feedstock/year
     input : array
-      Input quantities. 
+      Input quantities. Units: metric ton feedstock/year
     output_raw : array
-      Raw output quantities (before losses).
+      Raw output quantities (before losses). Units: gal biofuel/year
     output : array
-      Output quantities. 
+      Output quantities. Units: gal biofuel/year
     cost : array
       Costs.
     parameter : array
-      The technological parameterization.
+      The technological parameterization. Units vary; given in comments below
     """
 
-    return np.vstack([parameter[2]])
+    # JOBS: person-hours/gal biofuel
+    # parameter[2] units: person-hours/year
+    # output units: gal biofuel/year
+    emp = parameter[2] / output
+
+    # FOSSIL GHG: kg CO2-eq/gal biofuel
+    # parameter[0] units: kg CO2-eq/year
+    # output units: gal biofuel/year
+    ghg_foss = parameter[0] / output
+
+    # TOTAL GHG: kg CO2-eq/gal biofuel
+    # parameter[0] and parameter[1] units: kg CO2-eq/year
+    # output units: gal biofuel/year
+    ghg_tot = (parameter[0] + parameter[1]) / output
+
+    return np.stack([emp, ghg_foss, ghg_tot])
