fad9def304d0c202c9cc74247586a7863083d6ca
rhanes <rebecca.hanes@nrel.gov>
2022-05-17 14:44:01 -0600

    adds distribution parsing in tranches; not tested

--- src/tyche/Investments.py
+++ src/tyche/Investments.py
@@ -5,7 +5,9 @@ Investments in technologies.
 import numpy  as np
 import pandas as pd
 
+from .Distributions import parse_distribution
 from .IO    import make_table, read_table
+from .Designs import sampler
 from .Types import Evaluations
 
 
@@ -25,7 +27,7 @@ class Investments:
     "Category"   : np.str_   ,
     "Tranche"    : np.str_   ,
     "Scenario"   : np.str_   ,
-    "Amount"     : np.float64,
+    "Amount"     : np.str_   ,
     "Notes"      : np.str_   ,
   }
   _investments_dtypes = {
@@ -41,18 +43,23 @@ class Investments:
   def __init__(
     self                           ,
     path        = None             ,
+    uncertain   = False            ,
     tranches    = "tranches.csv"   ,
     investments = "investments.csv",
   ):
     """
     Parameters
     ----------
+    path : str
+      Path to directory where *tranches* and *investments* tables are saved.
+    uncertain : Boolean
+      Flag indicating whether probability distributions are present in the *tranches* table.
     tranches : str
       Filename for the *tranches* table.
     investments: str
       Filename for the *investments* table.
     """
-
+    self.uncertain = uncertain
     if path == None:
       self._make()
     else:
@@ -65,7 +72,12 @@ class Investments:
   def _read(self, path, tranches, investments):
     self.tranches    = read_table(path, tranches   , self._tranches_dtypes   , self._tranches_index   )
     self.investments = read_table(path, investments, self._investments_dtypes, self._investments_index)
-      
+
+  def compile(self):
+    """Parse any probability distributions in the tranches."""
+    self.compiled_tranches = self.tranches.copy()
+    self.compiled_tranches["Amount"] = self.compiled_tranches["Amount"].apply(parse_distribution)
+  
   def evaluate_tranches(self, designs, sample_count=1):
     """
     Evaluate the tranches of investment for a design.
@@ -77,19 +89,64 @@ class Investments:
     sample_count : int
       The number of random samples.
     """
+    # Parse any probability distributions in the tranches table
+    self.compile()
 
-    amounts = self.tranches.drop(
-      columns=["Notes"]
-    ).sum(
-      level=["Category", "Tranche"]
-    )
-    metrics = self.tranches.drop(
-      columns=["Amount", "Notes"]
-    ).join(
-      designs.evaluate_scenarios(sample_count).xs("Metric", level="Variable")
-    ).reorder_levels(
-      ["Category", "Tranche", "Scenario", "Sample", "Technology", "Index"]
-    )
+    # Check that only one of designs, investments contains uncertainty
+    if self.uncertain + designs.uncertain > 1:
+      print('Too much uncertainty. Remove probability distributions from tranches OR from technology data.')
+
+    if self.uncertain:
+      self.compiled_tranches["Samples"] = pd.Series(
+        sampler(
+          self.compiled_tranches.Amount,
+          sample_count
+          ).tolist(),
+        index=self.compiled_tranches.index
+      )
+      samples = self.compiled_tranches.explode("Samples")
+      samples["Amount"] = samples["Samples"]
+      samples["Sample"] = pd.Series(
+        np.tile(
+          np.arange(sample_count),
+          len(self.compiled_tranches.index)
+        ),
+        index=samples.index
+      )
+      samples.reset_index(
+        inplace=True
+      )
+      samples.set_index(
+        keys = ["Category", "Tranche", "Sample"],
+        inplace=True
+      )
+      amounts = samples.drop(
+        columns=["Samples", "Notes"]
+        ).sum(
+          level=["Category", "Tranche", "Sample"]
+        )
+      metrics = amounts.drop(
+        columns=["Amount"]
+      ).join(
+        designs.evaluate_scenarios(1).xs("Metric", level="Variable")
+      ).reorder_levels(
+        ["Category", "Tranche", "Scenario", "Sample", "Technology", "Index"]
+      )
+    
+    else:
+      amounts = self.compiled_tranches.drop(
+        columns=["Notes"]
+      ).sum(
+        level=["Category", "Tranche"]
+      )
+      metrics = self.compiled_tranches.drop(
+        columns=["Amount", "Notes"]
+      ).join(
+        designs.evaluate_scenarios(sample_count).xs("Metric", level="Variable")
+      ).reorder_levels(
+        ["Category", "Tranche", "Scenario", "Sample", "Technology", "Index"]
+      )
+
     return Evaluations(
       amounts = amounts,
       metrics = metrics,
