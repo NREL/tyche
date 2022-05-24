32dd88910b620b56992a70a2c584937fd8f7071d
rhanes <rebecca.hanes@nrel.gov>
2022-05-24 15:24:00 -0600

    refactors Evaluator init to pull directly from output of evaluate_tranches

--- src/tyche/Evaluator.py
+++ src/tyche/Evaluator.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 from scipy.interpolate import interp1d
 
+from .Types import Evaluations
+
 
 class Evaluator:
   """
@@ -26,21 +28,27 @@ class Evaluator:
     Interpolation functions for technology metrics.
   """
 
-  def __init__(self, tranches, summary):
+  def __init__(self, tranches):
     """
     Parameters
     ----------
-    tranches : DataFrame
-      The tranches of investment.
-    summary : DataFrame
-      The summary of evaluating the tranches.
+    tranches : Evaluations
+      Output of the evaluate_tranches method in Investments class. Named tuple with
+      the following elements:
+        amounts   : DataFrame
+        metrics   : Data Frame
+        summary   : Data Frame
+        uncertain : Boolean
     """
 
-    self.amounts    = tranches.groupby(["Category", "Tranche"]).sum()
-    self.categories = summary.reset_index()["Category"].unique()
-    self.metrics    = summary.reset_index()["Index"   ].unique()
-    self.units      = summary[["Units"]].groupby("Index").max()
-    self.raw        = summary.join(self.amounts)
+    if tranches.uncertain:
+      self.amounts    = tranches.amounts.groupby(["Category", "Tranche", "Sample"]).sum()
+    else:
+      self.amounts    = tranches.amounts.groupby(["Category", "Tranche"]).sum()
+    self.categories = tranches.summary.reset_index()["Category"].unique()
+    self.metrics    = tranches.summary.reset_index()["Index"   ].unique()
+    self.units      = tranches.summary[["Units"]].groupby("Index").max()
+    self.raw        = tranches.summary.join(self.amounts)
     self.interpolators = self.raw.groupby(
       ["Category", "Index", "Sample"]
     ).apply(
@@ -55,8 +63,8 @@ class Evaluator:
       "Interpolator"
     )
     self.max_amount = self.amounts.groupby("Category").max()
-    self.min_metric = summary.groupby(["Category", "Index"]).min(numeric_only = True).groupby("Index").sum()
-    self.max_metric = summary.groupby(["Category", "Index"]).max(numeric_only = True).groupby("Index").sum()
+    self.min_metric = tranches.summary.groupby(["Category", "Index"]).min(numeric_only = True).groupby("Index").sum()
+    self.max_metric = tranches.summary.groupby(["Category", "Index"]).max(numeric_only = True).groupby("Index").sum()
 
   def evaluate(self, amounts):
     """
@@ -160,7 +168,7 @@ class Evaluator:
       ).drop(
           columns = ["Tranche","Technology","Sample", "Units"]
       ).apply(
-          np.mean, axis=1
+          statistic, axis=1
       ).rename(
           "Value"
       )
