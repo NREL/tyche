0641357b8647c6d3e76e9d5de931b8c24da4fa49
rhanes <rebecca.hanes@nrel.gov>
2022-12-27 14:36:02 -0700

    updates release version in Sphinx conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, National Renewable Energy Laboratory'
 author = 'National Renewable Energy Laboratory'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.2'
 
 
 # -- General configuration ---------------------------------------------------
