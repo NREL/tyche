3cad3d5d6b3845f5af798ceba55805abcb60fa91
rhanes <rebecca.hanes@nrel.gov>
2023-02-13 14:49:14 -0700

    #130 adds blank technology model templates; incomplete/non functional

---
+++ src/technology/technology_a_model.py
@@ -0,0 +1,90 @@
+"""
+Template for technology model file.
+
+Corresponds to Technology A in the template decision context.
+"""
+
+import numpy as np
+
+def capital_cost(scale, parameter):
+  """
+  Capital cost function.
+
+  Parameters
+  ----------
+  scale : float
+    The scale of operation.
+  parameter : array
+    The technological parameterization.
+  """
+  # Stack the capital cost values into a single array to return.
+  return np.stack([0.0])
+
+
+def fixed_cost(scale, parameter):
+  """
+  Capital cost function.
+
+  Parameters
+  ----------
+  scale : float
+    The scale of operation.
+  parameter : array
+    The technological parameterization.
+  """
+  # Stack the fixed cost values into a single array to return.
+  return np.stack([0.0])
+
+
+def production(scale, capital, lifetime, fixed, input, parameter):
+  """
+  Production function.
+
+  Parameters
+  ----------
+  scale : float
+    The scale of operation.
+  capital : array
+    Capital costs.
+  lifetime : float
+    Technology lifetime.
+  fixed : array
+    Fixed costs.
+  input : array
+    Input quantities. 
+  parameter : array
+    The technological parameterization.
+  """
+  # Stack the calculated output values into a single array to return.
+  return np.stack([0.0])
+
+
+def metrics(scale, capital, lifetime, fixed, input_raw, input, input_price, output_raw, output, cost, parameter):
+  """
+  Metrics function.
+
+  Parameters
+  ----------
+  scale : float
+    The scale of operation.
+  capital : array
+    Capital costs.
+  lifetime : float
+    Technology lifetime.
+  fixed : array
+    Fixed costs.
+  input_raw : array
+    Raw input quantities (before losses).
+  input : array
+    Input quantities. 
+  output_raw : array
+    Raw output quantities (before losses).
+  output : array
+    Output quantities. 
+  cost : array
+    Costs.
+  parameter : array
+    The technological parameterization.
+  """
+  # Stack the metric values into a single array to return.
+  return np.stack([0.0])
