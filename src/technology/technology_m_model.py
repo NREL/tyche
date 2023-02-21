733c60fd1ae1232ec6cc265262399b9d3a17a785
rhanes <rebecca.hanes@nrel.gov>
2023-02-21 13:17:59 -0700

    #130 completes rough draft of template; NOT TESTED

--- src/technology/technology_m_model.py
+++ src/technology/technology_m_model.py
@@ -18,7 +18,9 @@ def capital_cost(scale, parameter):
     The technological parameterization.
   """
   # Stack the capital cost values into a single array to return.
-  return np.stack([0.0])
+  return np.stack([parameter[0] * scale**0.6,
+                   parameter[1] * scale**0.6,
+                   parameter[2] * scale**0.6])
 
 
 def fixed_cost(scale, parameter):
@@ -32,8 +34,10 @@ def fixed_cost(scale, parameter):
   parameter : array
     The technological parameterization.
   """
+  # Fixed costs are being modeled as a fraction of the
+  # total capital cost.  
   # Stack the fixed cost values into a single array to return.
-  return np.stack([0.0])
+  return np.stack([0.1 * (parameter[0] + parameter[1] + parameter[2]) * scale])
 
 
 def production(scale, capital, lifetime, fixed, input, parameter):
@@ -56,7 +60,9 @@ def production(scale, capital, lifetime, fixed, input, parameter):
     The technological parameterization.
   """
   # Stack the calculated output values into a single array to return.
-  return np.stack([0.0])
+  return np.stack([parameter[8] * input[2],
+                   parameter[9] * input[1],
+                   parameter[10] * input[2]])
 
 
 def metrics(scale, capital, lifetime, fixed, input_raw, input, input_price, output_raw, output, cost, parameter):
@@ -86,5 +92,10 @@ def metrics(scale, capital, lifetime, fixed, input_raw, input, input_price, outp
   parameter : array
     The technological parameterization.
   """
+  _annualized_cost = capital[0] + capital[1] + capital[2] + fixed*lifetime + input_price[0] * input[0] + input_price[1] * input[1] + input_price[2] * input[2]
+  _labor_per_unit_F = (parameter[6] * output[0] + parameter[7] * (output[1] + output[2]))/output[0]
+  _impact_per_unit_F = (parameter[3] * input_raw[0] + parameter[4] * input_raw[1] + parameter[5] + input_raw[2]) / output[0]
   # Stack the metric values into a single array to return.
-  return np.stack([0.0])
+  return np.stack([_annualized_cost,
+                   _labor_per_unit_F,
+                   _impact_per_unit_F])
