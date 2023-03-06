#130 corrects annualized cost calculation and stacked return values

src/technology/technology_m_model.py
@@ -18,11 +18,11 @@ def capital_cost(scale, parameter):
     The technological parameterization.
   """
   # Stack the capital cost values into a single array to return.
-  return np.stack([parameter[0] * scale**0.6,
+  return np.stack((parameter[0] * scale**0.6,
                    parameter[1] * scale**0.6,
                    parameter[2] * scale**0.6,
                    parameter[3] * scale**0.6,
-                   parameter[4] * scale**0.6])
+                   parameter[4] * scale**0.6))
 
 
 def fixed_cost(scale, parameter):
@@ -39,7 +39,7 @@ def fixed_cost(scale, parameter):
   # Fixed costs are being modeled as a fraction of the
   # total capital cost.  
   # Stack the fixed cost values into a single array to return.
-  return np.stack([0.1 * (parameter[0] + parameter[1] + parameter[2] + parameter[3] + parameter[4]) * scale])
+  return np.stack(0.1 * (parameter[0] + parameter[1] + parameter[2] + parameter[3] + parameter[4]) * scale)
 
 
 def production(scale, capital, lifetime, fixed, input, parameter):
@@ -62,9 +62,9 @@ def production(scale, capital, lifetime, fixed, input, parameter):
     The technological parameterization.
   """
   # Stack the calculated output values into a single array to return.
-  return np.stack([parameter[13] * input[2],
+  return np.stack((parameter[13] * input[2],
                    parameter[14] * input[1],
-                   parameter[14] * input[2]])
+                   parameter[14] * input[2]))
 
 
 def metrics(scale, capital, lifetime, fixed, input_raw, input, input_price, output_raw, output, cost, parameter):
@@ -94,12 +94,12 @@ def metrics(scale, capital, lifetime, fixed, input_raw, input, input_price, outp
   parameter : array
     The technological parameterization.
   """
-  _annualized_cost_per_unit_F = (capital[0] + capital[1] + capital[2] + capital[3] + capital[4] + fixed*lifetime + input_price[0] * input_raw[0] + input_price[1] * input_raw[1] + input_price[2] * input_raw[2])/output[0]
+  _annualized_cost_per_unit_F = (capital[0] + capital[1] + capital[2] + capital[3] + capital[4] + fixed*lifetime[0] + input_price[0] * input_raw[0] + input_price[1] * input_raw[1] + input_price[2] * input_raw[2])/output[0]
   _labor_per_unit_F = (parameter[8] * output_raw[0] + parameter[9] * (output_raw[1] + output_raw[2]))/output[0]
   _impact_per_unit_F = (parameter[5] * input_raw[0] + parameter[6] * input_raw[1] + parameter[7] + input_raw[2]) / output[0]
-  _overall_efficiency = np.mean([input_raw[0] / input[0], input_raw[1] / input[1], input_raw[2] / input[2], output_raw[0] / output[0], output_raw[1] / output[1], output_raw[2] / output[2]])
+  _overall_efficiency = output[0] / (input_raw[0] + input_raw[1] + input_raw[2])
   # Stack the metric values into a single array to return.
-  return np.stack([_annualized_cost_per_unit_F,
+  return np.stack((_annualized_cost_per_unit_F,
                    _labor_per_unit_F,
                    _impact_per_unit_F,
-                   _overall_efficiency])
+                   _overall_efficiency))