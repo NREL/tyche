6155eda3cb2a8f4bca94621d37bc78312a99c806
rhanes <rebecca.hanes@nrel.gov>
2023-03-20 15:51:08 -0600

    #130 switches to delta metrics and expands visualization options

--- src/technology/technology_m_model.py
+++ src/technology/technology_m_model.py
@@ -62,7 +62,7 @@ def production(scale, capital, lifetime, fixed, input, parameter):
     The technological parameterization.
   """
   # Stack the calculated output values into a single array to return.
-  return np.stack((parameter[13] * input[2],
+  return np.stack((parameter[13] * parameter[14] * input[2],
                    parameter[14] * input[1],
                    parameter[14] * input[2]))
 
@@ -94,10 +94,10 @@ def metrics(scale, capital, lifetime, fixed, input_raw, input, input_price, outp
   parameter : array
     The technological parameterization.
   """
-  _annualized_cost_per_unit_F = (capital[0] + capital[1] + capital[2] + capital[3] + capital[4] + fixed*lifetime[0] + input_price[0] * input_raw[0] + input_price[1] * input_raw[1] + input_price[2] * input_raw[2])/output[0]
-  _labor_per_unit_F = (parameter[8] * output_raw[0] + parameter[9] * (output_raw[1] + output_raw[2]))/output[0]
-  _impact_per_unit_F = (parameter[5] * input_raw[0] + parameter[6] * input_raw[1] + parameter[7] + input_raw[2]) / output[0]
-  _overall_efficiency = output[0] / (input_raw[0] + input_raw[1] + input_raw[2])
+  _annualized_cost_per_unit_F = (capital[0] + capital[1] + capital[2] + capital[3] + capital[4] + fixed*lifetime[0] + input_price[0] * input_raw[0] + input_price[1] * input_raw[1] + input_price[2] * input_raw[2])/output[0] - parameter[15]
+  _labor_per_unit_F = (parameter[8] * output_raw[0] + parameter[9] * (output_raw[1] + output_raw[2]))/output[0] - parameter[16]
+  _impact_per_unit_F = (parameter[5] * input_raw[0] + parameter[6] * input_raw[1] + parameter[7] * input_raw[2]) / output[0] - parameter[17]
+  _overall_efficiency = output[0] / (input_raw[0] + input_raw[1] + input_raw[2]) - parameter[18]
   # Stack the metric values into a single array to return.
   return np.stack((_annualized_cost_per_unit_F,
                    _labor_per_unit_F,
