ccfef88db88d2474a7cd86a59e24b4665bf3dbbc
rhanes <rebecca.hanes@nrel.gov>
2023-09-13 13:10:03 -0600

    #150 initial version of function to generate distribution from elicitation response data

---
+++ src/tyche/ElicitationProcessing.py
@@ -0,0 +1,86 @@
+import sys
+
+import numpy as np
+
+def generate_distn(
+    response_df,
+    expert_weights = None,
+    expert_column = 'expert_id',
+    response_columns = {'param': ['param_min','param_mode','param_max']},
+    measures = ['min', 'mode', 'max'],
+):
+    """
+    Convert raw elicitation responses into triangular distribution parameters.
+    
+    Reminder that scipy.stats.triang uses the following parameters:
+    (c, loc=0, scale=1, size=1, random_state=None)
+    c is the peak of the distribution (mode, NOT mean or median)
+    scale is the width of the distribution (max - min)
+    loc is the minimum of the distribution
+    
+    Parameters
+    ----------
+    response_df: DataFrame
+        DataFrame containing elicited responses. No default value.
+        Column names listed in response_columns must appear in this DataFrame.
+        Otherwise the method will break.
+    
+    expert_weights: Dict
+        Weights to apply to individual expert responses when calculating an
+        aggregate probability distribution.
+        Keys (str) are expert ID information contained in response_df.
+        Values (float) are weights to apply to each expert's responses.
+        Weights are normalized to sum to 1 before use.
+        All experts without a weight provided are assigned a weight of zero,
+        that is, are excluded from the aggregate distribution parameters.
+        If expert_weights is None (the default value), then experts are
+        weighted equally.
+    
+    expert_column: str
+        Name of the column with expert ID information.
+        Values in this column must match up with the ID information provided in 
+        the expert_weights dictionary.
+    
+    measures: List(str)
+        List of statistical measures elicited.
+        For triangular distributions, measures must include at least
+        the minimum, maximum, and mode (most likely/most common).
+        Required values: min, max, mode
+    
+    response_columns: Dict
+        Dictionary listing column names with elicited responses, by parameter.
+        Keys are parameter names.
+        values are lists of columns with responses for that parameter.
+        Each column in the list must correspond to the measures listed in 
+        the measures parameter.
+    
+    Returns
+    -------
+        string defining triangular distribution(s) for elicited parameters
+    """
+    # If the measures can't be used as min, mode, and max, throw an error
+    if not all(_m in measures for _m in ['min', 'mode', 'max']):
+        sys.exit('parameter error: measures must contain a minimum, maximum, and central measure')
+    
+    # Get list of all experts
+    _all_experts = response_df[expert_column].drop_duplicates().values
+    # If weights have been provided,
+    if expert_weights:
+        # For any expert not provided a weight, assign a zero weight which
+        # excludes that expert's responses from the weighted average response
+        for _e in _all_experts:
+            if _e not in expert_weights:
+                expert_weights[_e] = 0.0
+        # then add a column with the expert weights to response_df
+        response_df['weights'] = response_df['expert_id'].map(expert_weights)
+    else:
+        # If weights have not been provided, all experts get an equal weighting
+        response_df['weights'] = 1.0
+    
+    # Apply normalized weights to the responses, store in separate variable
+    parameters_agg = {}
+    for _p in response_columns.keys():
+        _r = [np.average(response_df[f'{_c}'], weights=response_df['weights']) for _c in [colname for value in response_columns.values() for colname in value]]
+        parameters_agg[f'{_p}_dist'] = f'st.triang(c={np.round(_r[1], decimals=2)}, loc={np.round(_r[2]-_r[0], decimals=2)}, scale={np.round(_r[0], decimals=2)})'
+        
+    return parameters_agg
