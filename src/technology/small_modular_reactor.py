c8d644b5ebbafbda39d3dd837cff1f0ca738ce5c
rhanes <rebecca.hanes@nrel.gov>
2025-09-22 09:48:31 -0500

    populates Tyche functions with nuclear_stride calls; NOT TESTED

--- src/technology/small_modular_reactor.py
+++ src/technology/small_modular_reactor.py
@@ -19,8 +19,36 @@ def capital_cost(scale, parameter):
   parameter : array
     The technological parameterization.
   """
+  # See the input dataset for parameter definitions, units, and ranges
+  all_results = calculate_final_result(
+    reactor_type = 'Concept B',
+    n_th = parameter[15],
+    f_22 = parameter[19],
+    f_2321 = parameter[20],
+    land_cost_per_acre_0 = parameter[5],
+    RB_grade_0 = parameter[10],
+    BOP_grade_0 = parameter[9],
+    num_orders = parameter[14],
+    design_completion_0 = parameter[1],
+    ae_exp_0 = parameter[3],
+    N_AE = parameter[16],
+    ce_exp_0 = parameter[4],
+    N_cons = parameter[18],
+    mod_0 = parameter[11],
+    Design_Maturity_0 = parameter[0],
+    proc_exp_0 = parameter[2],
+    N_proc = parameter[17],
+    standardization_0 = parameter[12],
+    interest_rate_0 = parameter[8],
+    startup_0 = parameter[13],
+    om_fixed = parameter[22],
+    om_variable = parameter[21],
+    spent_fuel_cost = parameter[23],
+  )
   # Stack the capital cost values into a single array to return.
-  return np.stack()
+  # The second element of the tuple returned by calculate_final_result is the
+  # total overnight capital cost for the entire reactor.
+  return np.stack(all_results[1])
 
 
 def fixed_cost(scale, parameter):
@@ -34,8 +62,33 @@ def fixed_cost(scale, parameter):
   parameter : array
     The technological parameterization.
   """
+  all_results = calculate_final_result(
+    reactor_type = 'Concept B',
+    n_th = parameter[15],
+    f_22 = parameter[19],
+    f_2321 = parameter[20],
+    land_cost_per_acre_0 = parameter[5],
+    RB_grade_0 = parameter[10],
+    BOP_grade_0 = parameter[9],
+    num_orders = parameter[14],
+    design_completion_0 = parameter[1],
+    ae_exp_0 = parameter[3],
+    N_AE = parameter[16],
+    ce_exp_0 = parameter[4],
+    N_cons = parameter[18],
+    mod_0 = parameter[11],
+    Design_Maturity_0 = parameter[0],
+    proc_exp_0 = parameter[2],
+    N_proc = parameter[17],
+    standardization_0 = parameter[12],
+    interest_rate_0 = parameter[8],
+    startup_0 = parameter[13],
+    om_fixed = parameter[22],
+    om_variable = parameter[21],
+    spent_fuel_cost = parameter[23],
+  )
   
-  return np.stack()
+  return np.stack(all_results[4])
 
 
 def production(scale, capital, lifetime, fixed, input, parameter):
@@ -57,8 +110,34 @@ def production(scale, capital, lifetime, fixed, input, parameter):
   parameter : array
     The technological parameterization.
   """
-  # Stack the calculated output values into a single array to return.
-  return np.stack()
+  all_results = calculate_final_result(
+    reactor_type = 'Concept B',
+    n_th = parameter[15],
+    f_22 = parameter[19],
+    f_2321 = parameter[20],
+    land_cost_per_acre_0 = parameter[5],
+    RB_grade_0 = parameter[10],
+    BOP_grade_0 = parameter[9],
+    num_orders = parameter[14],
+    design_completion_0 = parameter[1],
+    ae_exp_0 = parameter[3],
+    N_AE = parameter[16],
+    ce_exp_0 = parameter[4],
+    N_cons = parameter[18],
+    mod_0 = parameter[11],
+    Design_Maturity_0 = parameter[0],
+    proc_exp_0 = parameter[2],
+    N_proc = parameter[17],
+    standardization_0 = parameter[12],
+    interest_rate_0 = parameter[8],
+    startup_0 = parameter[13],
+    om_fixed = parameter[22],
+    om_variable = parameter[21],
+    spent_fuel_cost = parameter[23],
+  )
+  # Return: MWh electricity produced in a year
+  # Accounts for capacity factor
+  return np.stack(all_results[5])
 
 
 def metrics(scale, capital, lifetime, fixed, input_raw, input, input_price, output_raw, output, cost, parameter):
@@ -88,6 +167,36 @@ def metrics(scale, capital, lifetime, fixed, input_raw, input, input_price, outp
   parameter : array
     The technological parameterization.
   """
-  
-  # Stack the metric values into a single array to return.
-  return np.stack()
+  all_results = calculate_final_result(
+    reactor_type = 'Concept B',
+    n_th = parameter[15],
+    f_22 = parameter[19],
+    f_2321 = parameter[20],
+    land_cost_per_acre_0 = parameter[5],
+    RB_grade_0 = parameter[10],
+    BOP_grade_0 = parameter[9],
+    num_orders = parameter[14],
+    design_completion_0 = parameter[1],
+    ae_exp_0 = parameter[3],
+    N_AE = parameter[16],
+    ce_exp_0 = parameter[4],
+    N_cons = parameter[18],
+    mod_0 = parameter[11],
+    Design_Maturity_0 = parameter[0],
+    proc_exp_0 = parameter[2],
+    N_proc = parameter[17],
+    standardization_0 = parameter[12],
+    interest_rate_0 = parameter[8],
+    startup_0 = parameter[13],
+    om_fixed = parameter[22],
+    om_variable = parameter[21],
+    spent_fuel_cost = parameter[23],
+  )
+  # Metrics: construction duration (months), levelized net overnight capital cost,
+  # levelized net capital investment, total overnight cost per kWe, total capital
+  # investment cost per kWh
+  return np.stack([all_results[3],
+                   all_results[1],
+                   all_results[2],
+                   all_results[6],
+                   all_results[7]])
