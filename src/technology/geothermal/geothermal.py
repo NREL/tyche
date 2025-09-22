b3a957a3b64900d2736736c9ebb869e09718b36a
Abdelrahman Ayad <abdelrahman.ayad@nrel.gov>
2025-09-21 23:30:32 -0600

    geothermal initial model

---
+++ src/technology/geothermal/geothermal.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+import numpy             as np
+import matplotlib.pyplot as pl
+import pandas            as pd
+import seaborn           as sb
+from pathlib import Path
+
+sys.path.insert(0, os.path.abspath("../../../src"))
+
+import tyche             as ty
+
+cur_dir =  Path(__file__).parent.absolute()
+technology_dir = cur_dir.parent
+src_dir = technology_dir.parent
+
+atb_dir = os.path.join(src_dir, 'atb')
+
+geo_parameters = {'Case': 'Market',
+              'TaxCreditCase': 'ITC',
+              'CRPYears': 20,
+              'Technology': 'Geothermal',
+              'DisplayName': 'Geothermal - Hydro / Flash',
+              'Scenario': 'Moderate',
+              'variable': 2022
+              }
+
+designs = ty.Designs(path = ".",
+                    name = 'geothermal.xlsx')
+
+atb_geo = ty.ATB(path = atb_dir,
+             atb_tech = 'geothermal',
+             tech_filename = 'geothermal_atb.csv',
+             parameters = geo_parameters)
